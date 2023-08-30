c872d826c1018d50750779af51f50fcda44b2ca6
Leandro Santos <lds.leosantos@gmail.com>
2023-08-30 07:31:28 -0300

    285 - Bloquear a exclusão do registro que é usado como chave estrangeira

--- app/adms/Models/AdmsDeleteSitsUsers.php
+++ app/adms/Models/AdmsDeleteSitsUsers.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\adms\Models;
+use App\Adms\Models\helper\AdmsRead;
 
 if(!defined('C8L6K7E')){
     header("Location: /");
@@ -24,11 +25,7 @@ class AdmsDeleteSitsUsers
     /** @var array|null $resultBd Recebe os registros do banco de dados */
     private ?array $resultBd;
 
-    /** @var string $delDirectory Recebe o endereço para apagar o diretório */
-    private string $delDirectory;
-
-    /** @var string $delImg Recebe o endereço para apagar a imagem */
-    private string $delImg;
+   
 
     /**
      * @return bool Retorna true quando executar o processo com sucesso e false quando houver erro
@@ -42,20 +39,23 @@ class AdmsDeleteSitsUsers
     {
         $this->id = (int) $id;
 
-        if($this->viewSitUser()){
+        if( ($this->viewSitUser()) and ($this->checkStatusUsed() )){
             $deleteUser = new \App\adms\Models\helper\AdmsDelete();
             $deleteUser->exeDelete("adms_sists_users", "WHERE id =:id", "id={$this->id}");
     
-            if ($deleteUser->getResult()) {
-                $_SESSION['msg'] = "<p style='color: green;'>Situação apagada com sucesso!</p>";
-                $this->result = true;
-            } else {
-                $_SESSION['msg'] = "<p style='color: #f00;'>Erro: Situação não apagada com sucesso!</p>";
-                $this->result = false;
-            }
-        }else{
-            $this->result = false;
-        }
+            var_dump($deleteUser);
+
+             if ($deleteUser->getResult()) {
+                 $_SESSION['msg'] = "<p style='color: green;'>Situação apagada com sucesso!</p>";
+                 $this->result = true;
+             } else {
+                 $_SESSION['msg'] = "<p style='color: #f00;'>Erro: Situação não apagada com sucesso!</p>";
+                 $this->result = false;
+             }
+          }
+          else{
+             $this->result = false;
+          }
         
     }
 
@@ -78,5 +78,24 @@ class AdmsDeleteSitsUsers
             $_SESSION['msg'] = "<p style='color: #f00'>Erro: Usuário não encontrado!</p>";
             return false;
         }
+    }
+
+
+    private function checkStatusUsed(): bool
+    {
+        $viewUserAdd  = new AdmsRead();
+        $viewUserAdd->fullRead("SELECT id FROM adms_users 
+        WHERE  adms_sits_user_id =:adms_sits_user_id 
+        LIMIT :limit", "adms_sits_user_id={$this->id}&limit=1");
+            var_dump($viewUserAdd->getResult());
+
+        if($viewUserAdd->getResult()){
+            $_SESSION['msg'] = "<p style='color: #f00'>Erro: Situação em uso, nao pode ser apagada!</p>";
+            return false;
+        }else{
+            $_SESSION['msg'] = "<p style='color: green;'>Situação apagada com sucesso!</p>";
+
+            return true;
+         }
     }
 }
