1187cb31c8f06f95baa4b7349877e861a6fac313
Leandro Santos <lds.leosantos@gmail.com>
2023-09-12 13:25:35 -0300

    seçao 12 - Como integrar o sistema administrativo com o layout - 302 - integrar o layout formulario cadastrar ususario

--- app/adms/Views/users/addUser.php
+++ app/adms/Views/users/addUser.php
@@ -1,54 +1,113 @@
-<?php
-if(isset($this->data['form'])){
 
+
+<?php
+
+if (!defined('C8L6K7E')) {
+    header("Location: /");
+    die("Erro: Página não encontrada<br>");
+};
+if (isset($this->data['form'])) {
     $valorForm = $this->data['form'];
 }
 
 ?>
-<h1>Cadastrar Novo Usuario</h1>
-
-<?php if(isset($_SESSION['msg'])){
-    echo $_SESSION['msg'];
-    unset($_SESSION['msg']);
-} ?>
-
-<span id="msg"></span>
-<form action="" method="post" id="form-add-user">
-
-<label for="">Nome</label>
-<input type="text" name="name" id="name" required placeholder="Digite o Nome completo" value="<?php if(isset($valorForm['name'])) {echo $valorForm['name'];}    ?>"><br><br>
-
-<label for="">Email</label>
-<input type="email" name="email" id="email" required placeholder="Digite o email" value="<?php if(isset($valorForm['email'])) 
-{echo $valorForm['email'];}    ?>"><br><br>
-
-<label for="">User</label>
-<input type="text" name="user" id="user" required placeholder="Digite o nome do usuario" value="<?php if(isset($valorForm['user'])) 
-{echo $valorForm['user'];}    ?>"><br><br>
 
 
+<!-- Inicio do conteudo do administrativo -->
+<div class="wrapper">
+    <div class="row">
+        <div class="top-list">
+            <span class="title-content">Cadastrar usuario</span>
+            <div class="top-list-right">
+                <?php echo  "<a href='" . URLADM . "list-users/index/' class='btn-info' >Listar</a>"; ?>
 
-<label for="">Situação</label>
-<select name="adms_sits_user_id" id="adms_sits_user_id">
-    <option value="">Selecione</option>
-        <?php foreach ($this->data['select']['sit'] as $sit) {
-                extract($sit);
-                if((isset($valorForm['adms_sits_user_id'])) and ($valorForm['adms_sits_user_id'] == $id_sit)){
-                    echo " <option value='$id_sit' selected>$name_sit</option>";
-                }else{
-                    echo " <option value='$id_sit'>$name_sit</option>";
+            </div>
+        </div>
 
-                }
-              
-        } ?>
+        <div class="content-adm">
+            <?php
+            if (isset($_SESSION['msg'])) {
+                echo $_SESSION['msg'];
+                unset($_SESSION['msg']);
+            }
+            ?>
+            <span id="msg"></span>
 
-</select><br><br>
-<label for="">senha</label>
-<input type="password" name="password" required id="password" placeholder="Digite a senha" onkeyup="passwordStrength()" autocomplete="on" value="<?php if(isset($valorForm['password'])) {echo $valorForm['password'];}    ?>"><br><br>
-
-<span id="msgViewStrength"></span><br>
-<button type="submit" name="SendAddNewUser" value="Cadastrar">Cadastrar</button>
-</form>
+        </div>
 
 
 
+        <div class="content-adm">
+        <form method="POST" action="" id="form-add-user" class="form-adm">
+                <div class="row-input">
+
+                    <div class="column">
+                        <label class="title-input ">Nome<span class="text-danger">*</span> </label>
+                        <input type="text" name="name" id="name" required value="<?php if (isset($valorForm['name'])) {
+                                                                                        echo $valorForm['name'];
+                                                                                    }    ?>" class="input-adm" placeholder="Nome completo">
+                    </div>
+
+                    <div class="column">
+                        <label class="title-input">E-mail<span class="text-danger">*</span></label>
+                        <input type="email" name="email" id="email" value="<?php if (isset($valorForm['email'])) {
+                                                                                echo $valorForm['email'];
+                                                                            }    ?>" required class="input-adm" placeholder="Melhor e-mail">
+                    </div>
+
+                </div>
+
+                <div class="row-input">
+
+
+                    <div class="column">
+
+                        <label class="title-input">User</label>
+
+
+                        <input type="text" class="input-adm" name="user" id="user" required placeholder="Digite o nome do usuario" value="<?php if (isset($valorForm['user'])) {
+                                                                                                                                                echo $valorForm['user'];
+                                                                                                                                            }    ?>">
+                    </div>
+
+                    <div class="column">
+
+                        <label class="title-input">Senha<span class="text-danger">*</span></label>
+                        <input type="password" name="password" required id="password" class="input-adm" placeholder="Digite a senha" onkeyup="passwordStrength()" autocomplete="on" value="<?php if (isset($valorForm['password'])) {
+                                                                                                                                                                                                echo $valorForm['password'];
+                                                                                                                                                                                            }    ?>">
+
+                    </div>
+                </div>
+
+
+
+                <div class="row-input">
+
+                    <div class="column">
+                        <label class="title-input">Situacao<span class="text-danger">*</span></label>
+                        <select name="adms_sits_user_id" id="adms_sits_user_id" class="input-adm">
+
+                            <option value="">Selecione  </option>
+
+                            <?php foreach ($this->data['select']['sit'] as $sit) {
+                                extract($sit);
+                                if ((isset($valorForm['adms_sits_user_id'])) and ($valorForm['adms_sits_user_id'] == $id_sit)) {
+                                    echo " <option value='$id_sit' selected>$name_sit</option>";
+                                } else {
+                                    echo " <option value='$id_sit'>$name_sit</option>";
+                                }
+                            } ?>
+                        </select>
+                    </div>
+                </div>
+
+
+                <span id="msgViewStrength"></span><br>
+                <button type="submit" name="SendAddNewUser"  class="btn btn-success" value="Cadastrar">Cadastrar</button>
+
+            </form>
+        </div>
+    </div>
+</div>
+<!-- Fim do conteudo do administrativo -->
